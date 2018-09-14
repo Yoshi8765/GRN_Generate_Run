ff990e00c55cf68e439a480336f1379712a58a77
Zack McNulty <zmcnulty@uw.edu>
2018-09-14 13:17:37 -0700

    Revert "push broken model for game"
    This reverts commit 25434d1e2d65ccb2d38fcfad47dbacf868b73bf0.

--- zack_kateka/Biotapestry.py
+++ zack_kateka/Biotapestry.py
@@ -1,16 +1,7 @@
 
 import tellurium as te
-from GetModel import convert_to_antimony
-from GetModel import Gene
 
 # From Biotapestry: Export > Export to SBML
-def convert_biotapestry_to_antimony(csv_filename, num_genes, init_param):
-    f = open(csv_filename)
-    all_genes = []
-    
-    for line in f:
-        words = line.split(",")
-        if words[0] == "general":
+def convert_biotapestry_to_antimony(SBML_filename):
+    return te.sbmlToAntimony(SBML_filename)
 
-
-print(convert_biotapestry_to_antimony("pathway_biotapestry.csv",8,init_param = [1,1,1,1,1,1,1]))
