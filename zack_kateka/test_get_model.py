49ec36c071f2fe13aea9a528d03d107094f29a50
Zack McNulty <zmcnulty@uw.edu>
2018-09-14 14:12:41 -0700

    made broken network

--- zack_kateka/test_get_model.py
+++ zack_kateka/test_get_model.py
@@ -15,16 +15,20 @@ from Biotapestry import convert_biotapestry_to_antimony
 
 start = time.time()
 
-ant_str = get_model(3, reg_probs=[0.3, 0.3, 0.2, 0.1, 0.1], reachability=1, init_params = [1]*7, param_std = 0, model_name = "test") 
-ant_str2 = convert_biotapestry_to_antimony("test_biotapestry.csv",3,[1]*7 , model_name = "test")
+ant_str = convert_biotapestry_to_antimony("Biotapestry/8gene_broken.csv", 8, [1]*7)
 
-print (ant_str)
 
-print ("-"*40)
-print ("\n\n\n")
-print (ant_str2)
-print ("-"*40)
-print (ant_str2 == ant_str)
+
+#ant_str = get_model(3, reg_probs=[0.3, 0.3, 0.2, 0.1, 0.1], reachability=1, init_params = [1]*7, param_std = 0, model_name = "test") 
+#ant_str2 = convert_biotapestry_to_antimony("test_biotapestry.csv",3,[1]*7 , model_name = "test")
+
+#print (ant_str)
+
+#print ("-"*40)
+#print ("\n\n\n")
+#print (ant_str2)
+#print ("-"*40)
+#print (ant_str2 == ant_str)
 
 #r=te.loada(ant_str)
 #r.reset()
