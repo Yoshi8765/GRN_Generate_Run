33ec29ac4a8e003efbf372842725fb0e83c7c665
Yoshi8765 <yoshigoto95@gmail.com>
2018-09-14 14:59:18 -0700

    +stuff

--- zack_kateka/RunModel.py
+++ zack_kateka/RunModel.py
@@ -6,71 +6,73 @@ import tellurium as te
 import numpy as np
 import math
 from matplotlib import pyplot as plt
-import pkg_resources
+import imp
 
-# TODO: check the filesave paths
+GRAPH_LABEL_FONTSIZE = 8
+GRAPH_TITLE_FONTSIZE = 10
 
-def run_model(antStr,model_name,noiseLevel,numGenes,runAttempts=5,tmax=60,savePath='\\model_output\\',showTimePlots='y',exportData='y',drawModel=['y','fdp']):
+def run_model(antStr,noiseLevel,exportData=[ [0],'P','y','y','y','y'],savePath='\\model_output\\',showTimePlots='y',seed=0,drawModel=['y','fdp'],runAttempts=5,tmax=60):
     """Asserts Antimony models will reach steady-state, generates visualizations, and exports data.
-
-    get_model is required to be in the same working directory as run_model.\n
-    get_model will be executed if run_model finds out that the current model does not reach steady-state.
 
     Arguments:
         antStr:
-        model_name:
         noiseLevel:
-        numGenes:
-        runAttempts:
-        tmax:
+        exportData:
+            [genesToExport,dataType ('P'rotein or 'M'rNA),csv,sbml,antimony,seed]
         savePath:
         showTimePlots:
-        exportData:
-        drawModel: Requires pygraphviz
+        seed:
+        drawModel: Requires pygraphviz and graphviz installed
+        runAttempts:
+        tmax:
+
 
     Outputs:
         model:
         result:
         resultNoisy:
     """
-    # Attempt to run RunModel up to runAttempts times with different generated models.
 
-    # Check that get_model can be used
-    try:
-        from GetModel import get_model
-    except:
-        print('Cannot find get_model to import.')
-
+    # Attempt to execute run_model up to runAttempts times with different generated models.
     for retry in range(runAttempts):
         # Load the Antimony string as a model
         try:
             model = te.loadAntimonyModel(antStr)
-            print('Success!') # TODO: Change to a better print
+            print('Successfully loaded in Antimony string as a model.')
         except Exception as error:
             ErrorPrinting(error)
-            raise RuntimeError("Failed to load antimony text due to parsing error. Check that your antStr is correct.")
-        # Check that the model reaches steady-state
+            raise RuntimeError("Failed to load antimony string due to parsing error. Check that your antStr is correct.")
+        # Check that the model reaches steady-state. If the model has any issues running, it will raise an Exception. The user should run get_model again.
+        plt.close('all')
+        model.reset()
+        tStep = int(math.ceil(tmax)*5)
         try:
-            plt.close('all')
-            model.reset()
-            tStep = int(math.ceil(tmax)*5)
-            result = model.simulate(0,tmax, tStep)
-        # If the model has any issues running, generate a new model and try again.
-        except Exception as error:
-            print "Failed to solve model."
-            ErrorPrinting(error)
-            model,antStr,GRNInt = get_model(num_genes, reg_probs, model_name, init_params,seed)
-            # TODO: Fix the above
-            break
+            model.steadyState()
+        except Exception:
+            try:
+                model.conservedMoietyAnalysis = True
+                model.steadyState()
+            except Exception as error:
+                print "Failed to reach steady-state or there is an Integrator error. Check your model or run get_model again."
+                ErrorPrinting(error)
+                raise error
+        # TODO: Error test to see behavior when it runs but doesn't reach steady state.
+
         else:
+            model.reset()
+            result = model.simulate(0,tmax,tStep)
+
+            numGenes = model.getNumFloatingSpecies()
+            model_name = model.getInfo().split("'modelName' : ")[1].split("\n")[0]
+
             # Specify (and make if necessary) a folder to save outputs to
             folderPath = os.getcwd() + '\\Random_GRNs\\'
-            modelPath = folderPath + model_name + '\\'
-            if os.path.exists(modelPath) == False:
+            filesPath = folderPath + model_name + '\\'
+            if os.path.exists(filesPath) == False:
                 if os.path.exists(folderPath) == False:
                     os.mkdir(folderPath)
-                os.mkdir(modelPath)
-                print('\nFolder created: ' + modelPath)
+                os.mkdir(filesPath)
+                print('\nFolder created: ' + filesPath)
 
             # Create results with artificial noise
             if noiseLevel != '0':
@@ -85,67 +87,77 @@ def run_model(antStr,model_name,noiseLevel,numGenes,runAttempts=5,tmax=60,savePa
                                 CurrVal = result[k,i] + np.random.normal(0,noiseLevel*result[k,i])
                             resultNoisy[k,i] = CurrVal
 
+
             # Create graphs
             if showTimePlots=='y':
-                makePlots(tmax,numGenes,result,resultNoisy)
-                # TODO: make plots generalized to how many outputs that the user wants
+                makePlots(tmax,numGenes,exportData,filesPath,result,noiseLevel,resultNoisy)
 
             # Export data
-            if exportData=='y':
-                Output(data, Name,model,seed,result,resultNoisy, fileName, antStr)
-                # TODO: Fix the above
+            Output(exportData,model,seed,result,noiseLevel,resultNoisy, filesPath, antStr)
 
-            # Draw the model (requires pygraphviz)
+            # Draw the model (requires pygraphviz module)
             if drawModel[0]=='y':
-                found = 0
-                installed_packages_list = sorted(["%s==%s" % (i.key, i.version) for i in pkg_resources.working_set])
-                for item in installed_packages_list:
-                    if item.startswith('pygraphviz'):
-                        model.draw(layout=str(drawModel[1]))
-                        found = 1
-                if found == 0:
+                try:
+                    imp.find_module('pygraphviz')
+                    model.draw(layout=str(drawModel[1]))
+                except ImportError:
                     print('pygraphviz is not installed!')
 
+            #returns the model, and result and/or resultNoisy arrays
             return(model,result,resultNoisy)
 
     raise RuntimeError('Could not create a working model for ' + str(runAttempts) + ' tries.')
 
 
-
-
-
-
-
-
-
-
 ###### Other Functions that GetModel uses ######
 
-def Output(data, Name,model,seed,result,resultNoisy, fileName, antStr):
-    for i in range(len(data)):
-        if i == 0 and data[i] == 'y':
-            writecsvFile(fileName + Name + '_Results.csv',model,result)
-        elif data[i] == 'y':
-            writecsvFile(fileName + Name + '_Noisy_Result.csv',model,resultNoisy)
-    if seed != 0:
-        fh = open(fileName + Name + '_Seed.txt', 'wb')
-        fh.write('Random Seed = ' + str(seed))
-    if seed == 0:
-        fh = open(fileName + Name + '_Seed.txt', 'wb')
-        fh.write('Random Seed not chosen.')
-    sbmlStr = model.getSBML()
-    te.saveToFile (fileName + Name + '_Model.xml', sbmlStr)
-    fh = open(fileName + Name + '_Antimony.txt', 'wb')
-    fh.write(str(antStr))
+def Output(exportData,model,seed,result,noiseLevel,resultNoisy,filesPath, antStr):
+    # export csv of results
+    if exportData[2]=='y':
+        outputSpecies = range(int(np.size(result,1)))
+        if exportData[0]!=0:
+            if exportData[1]=='P':
+                outputSpecies = [gene for gene in exportData[0]]
+            if exportData[1]=='M':
+                exportData = [x.__add__(1) for x in exportData[0]]
+                outputSpecies = [gene for gene in exportData[0]]
+        writecsvFile(filesPath + 'Results_Clean.csv',model,result[:,outputSpecies])
+        if noiseLevel != 0:
+            writecsvFile(filesPath + 'Noisy_Result.csv',model,result[:,outputSpecies])
+    # export SBML model text
+    if exportData[3]=='y':
+        sbmlStr = model.getSBML()
+        te.saveToFile (filesPath + 'OrigModel.xml', sbmlStr)
+    # export Antimony model text
+    if exportData[4]=='y':
+        fh = open(filesPath + 'OrigAntimony.txt', 'wb')
+        fh.write(str(antStr))
+    # export txt of seed number
+    if exportData[5]=='y':
+        if seed == 0:
+            fh = open(filesPath + 'Seed.txt', 'wb')
+            fh.write('Random Seed not chosen.')
+        else:
+            fh = open(filesPath + 'Seed.txt', 'wb')
+            fh.write('Random Seed = ' + str(seed))
+
     print('\nData Saved!\n')
 
-def writecsvFile (fileName, model, data):
+# function for exporting to csv
+def writecsvFile (exportData,filesPath, model, data):
     names = model.getFloatingSpeciesIds()
+    if exportData[0]!=0:
+        if exportData[1]=='P':
+            outputSpecies = [gene for gene in exportData[0]]
+        if exportData[1]=='M':
+            exportData = [x.__add__(1) for x in exportData[0]]
+            outputSpecies = [gene for gene in exportData[0]]
+    names = names[outputSpecies]
     header_string = names[0]
     for name in names[1-len(names):]:
         header_string = header_string + ',' + name
 
-    fh = open(fileName, "wb")
+    fh = open(filesPath, "wb")
     fh.write(header_string)
     fh.write('\n')
     for x in xrange(data.shape[0]):
@@ -158,9 +170,15 @@ def writecsvFile (fileName, model, data):
         fh.write('\n')
     fh.close()
 
-def makePlots(tmax,numGenes,result,resultNoisy):
+def makePlots(tmax,numGenes,exportData,filesPath,result,noiseLevel,resultNoisy):
     global GRAPH_LABEL_FONTSIZE
     global GRAPH_TITLE_FONTSIZE
+
+    if exportData[0]!=0:
+        numGenes = len(exportData[0])
+        outputGenes = [gene for gene in exportData[0]]
+
+    # create a plot for clean data
     jet = plt.get_cmap('jet')
     pickColorSpace = [int(np.floor(i)) for i in np.linspace(0,255,numGenes)]
     colors = jet(pickColorSpace)
@@ -168,7 +186,7 @@ def makePlots(tmax,numGenes,result,resultNoisy):
     vars = {'P':[],'M':[]}
     for e in vars.keys():
         plt.figure(1)
-        for k in np.arange(1,numGenes+1):
+        for k in np.arange(0,numGenes):
             vars[e].append(e + str(k))
             tStep = int(math.ceil(tmax)/20)
             tRange = np.arange(0,(int(math.ceil(tmax)))+ tStep,tStep)
@@ -178,7 +196,7 @@ def makePlots(tmax,numGenes,result,resultNoisy):
                 plt.grid(color='k', linestyle='-', linewidth=.4)
                 plt.ylabel('count',fontsize=GRAPH_LABEL_FONTSIZE)
                 plt.yticks(fontsize=GRAPH_LABEL_FONTSIZE)
-                plt.plot (result[:,0],result[:,(k-1)+1], label = vars[e][k-1],color=colors[k-1])
+                plt.plot (result[:,0],result[:,outputGenes[k]+1], label = vars[e][k],color=colors[k])
                 plt.yscale('log')
                 if tmax > 5000:
                     plt.xscale('log')
@@ -194,7 +212,7 @@ def makePlots(tmax,numGenes,result,resultNoisy):
                plt.xlabel('time(s)',fontsize=GRAPH_LABEL_FONTSIZE)
                plt.ylabel('count',fontsize=GRAPH_LABEL_FONTSIZE)
                plt.yticks(fontsize=GRAPH_LABEL_FONTSIZE)
-               plt.plot (result[:,0],result[:,(k-1)+numGenes+1], label = vars[e][k-1],color=colors[k-1])
+               plt.plot (result[:,0],result[:,outputGenes[k]+2], label = vars[e][k],color=colors[k])
                plt.yscale('log')
                if tmax > 5000:
                     plt.xscale('log')
@@ -206,53 +224,55 @@ def makePlots(tmax,numGenes,result,resultNoisy):
     manager = plt.get_current_fig_manager()
     manager.window.showMaximized()
     manager.window.showMinimized()
-    plt.savefig(modelPath + 'Simulation_Plot.png', dpi=400)
+    plt.savefig(filesPath + 'Simulation_Plot.png', dpi=400)
 
-    vars = {'P':[],'M':[]}
-    for e in vars.keys():
-        plt.figure(2)
-        for k in np.arange(1,numGenes+1):
-            vars[e].append(e + str(k))
-            if e=='P':
-                plt.subplot(2,1,1)
-                plt.title('Protein Count Vs. Time (Noisy)', fontsize=GRAPH_TITLE_FONTSIZE)
-                plt.grid(color='k', linestyle='-', linewidth=.4)
-                plt.ylabel('count',fontsize=GRAPH_LABEL_FONTSIZE)
-                plt.xlim(0,tmax)
-                plt.yticks(fontsize=GRAPH_LABEL_FONTSIZE)
-                plt.plot (resultNoisy[:,0],resultNoisy[:,(k-1)+1], label = vars[e][k-1],color=colors[k-1])
-                plt.legend(vars[e])
-                plt.yscale('log')
-                if tmax > 5000:
-                    plt.xscale('log')
-                    plt.xticks(fontsize=6)
-                else:
-                   plt.xticks(tRange, fontsize=GRAPH_LABEL_FONTSIZE)
-                plt.legend(loc='upper right',ncol=3, bbox_to_anchor=(1.13, 1.035), fontsize=GRAPH_LABEL_FONTSIZE)
-            if e=='M':
-                plt.subplot(2,1,2)
-                plt.title('mRNA Count Vs. Time (Noisy)', fontsize=GRAPH_TITLE_FONTSIZE)
-                plt.grid(color='k', linestyle='-', linewidth=.4)
-                plt.xlim(0,tmax)
-                plt.yticks(fontsize=GRAPH_LABEL_FONTSIZE)
-                plt.xlabel('time(s)',fontsize=GRAPH_LABEL_FONTSIZE)
-                plt.ylabel('count',fontsize=GRAPH_LABEL_FONTSIZE)
-                plt.plot (resultNoisy[:,0],resultNoisy[:,(k-1)+numGenes+1], label = vars[e][k-1],color=colors[k-1])
-                plt.legend(vars[e])
-                plt.yscale('log')
-                if tmax > 5000:
-                    plt.xscale('log')
-                    plt.xticks(fontsize=GRAPH_LABEL_FONTSIZE)
-                else:
-                    plt.xticks(tRange, fontsize=GRAPH_LABEL_FONTSIZE)
-                plt.legend(loc='upper right',ncol=3, bbox_to_anchor=(1.13, 1.035), fontsize=GRAPH_LABEL_FONTSIZE)
-    manager = plt.get_current_fig_manager()
-    manager.window.showMaximized()
-    manager.window.showMinimized()
-    plt.savefig(modelPath + 'Noisy Simulation_Plot.png', dpi=400)
-    print('\nPlots created!\n')
+    # create a plot for noisy data
+    if noiseLevel != 0:
+        vars = {'P':[],'M':[]}
+        for e in vars.keys():
+            plt.figure(2)
+            for k in np.arange(0,numGenes):
+                vars[e].append(e + str(k))
+                if e=='P':
+                    plt.subplot(2,1,1)
+                    plt.title('Protein Count Vs. Time (Noisy)', fontsize=GRAPH_TITLE_FONTSIZE)
+                    plt.grid(color='k', linestyle='-', linewidth=.4)
+                    plt.ylabel('count',fontsize=GRAPH_LABEL_FONTSIZE)
+                    plt.xlim(0,tmax)
+                    plt.yticks(fontsize=GRAPH_LABEL_FONTSIZE)
+                    plt.plot (resultNoisy[:,0],resultNoisy[:,outputGenes[k]+1], label = vars[e][k],color=colors[k])
+                    plt.legend(vars[e])
+                    plt.yscale('log')
+                    if tmax > 5000:
+                        plt.xscale('log')
+                        plt.xticks(fontsize=6)
+                    else:
+                       plt.xticks(tRange, fontsize=GRAPH_LABEL_FONTSIZE)
+                    plt.legend(loc='upper right',ncol=3, bbox_to_anchor=(1.13, 1.035), fontsize=GRAPH_LABEL_FONTSIZE)
+                if e=='M':
+                    plt.subplot(2,1,2)
+                    plt.title('mRNA Count Vs. Time (Noisy)', fontsize=GRAPH_TITLE_FONTSIZE)
+                    plt.grid(color='k', linestyle='-', linewidth=.4)
+                    plt.xlim(0,tmax)
+                    plt.yticks(fontsize=GRAPH_LABEL_FONTSIZE)
+                    plt.xlabel('time(s)',fontsize=GRAPH_LABEL_FONTSIZE)
+                    plt.ylabel('count',fontsize=GRAPH_LABEL_FONTSIZE)
+                    plt.plot (resultNoisy[:,0],resultNoisy[:,outputGenes[k]+2], label = vars[e][k],color=colors[k])
+                    plt.legend(vars[e])
+                    plt.yscale('log')
+                    if tmax > 5000:
+                        plt.xscale('log')
+                        plt.xticks(fontsize=GRAPH_LABEL_FONTSIZE)
+                    else:
+                        plt.xticks(tRange, fontsize=GRAPH_LABEL_FONTSIZE)
+                    plt.legend(loc='upper right',ncol=3, bbox_to_anchor=(1.13, 1.035), fontsize=GRAPH_LABEL_FONTSIZE)
+        manager = plt.get_current_fig_manager()
+        manager.window.showMaximized()
+        manager.window.showMinimized()
+        plt.savefig(filesPath + 'Noisy Simulation_Plot.png', dpi=400)
+        print('\nPlots created!\n')
 
-    #Attepts to show meaningful Errors
+    # Attepts to print meaningful Errors
 def ErrorPrinting(Error,customHeader=''):
     errorStr = str(Error)
     print('!'*len(errorStr))
