ee3e773b75471e810cb407458e34eb7ec5b453ae
Kateka Seth <katekaseth@gmail.com>
2018-09-17 11:01:15 -0700

    Add broken biotapestry csv

--- zack_kateka/test_change_biotap.py
+++ zack_kateka/test_change_biotap.py
@@ -5,6 +5,10 @@ Created on Mon Sep 17 09:44:50 2018
 @author: Kateka Seth
 """
 
-from change_biotapestry import add_connections_biotapestry
+from change_biotapestry import remove_biotapestry
 
-add_connections_biotapestry("Biotapestry/8gene_network.csv")
+remove=[(1,4),(3,5),(1,7),(1,8),(2,4),(7,7)]
+csv_filename="Biotapestry/8gene_network.csv"
+csv_newfile="Biotapestry/8gene_broken.csv"
+
+remove_biotapestry(remove, csv_filename, csv_newfile)
