16a4e48e54d3b3fb669a386c2c2af1da6a430bc3
Kateka Seth <katekaseth@gmail.com>
2018-09-18 12:02:17 -0700

    remove dup files

--- zack_kateka/playing_game/the_game.py
+++ zack_kateka/playing_game/the_game.py
@@ -4,7 +4,8 @@ Created on Mon Sep 17 12:30:55 2018
 
 @author: Kateka Seth
 """
-
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from change_biotapestry import remove_biotapestry
 from change_biotapestry import add_biotapestry
 from GetModel import get_model
@@ -17,7 +18,7 @@ import antimony
 csv_filename="../Biotapestry/8gene_broken.csv"
 csv_newfile="../Biotapestry/8gene_test.csv"
 #init_params = ['d_p', 'd_m' , 'L' , 'Vm' , 'a_p' , 'H', 'K']
-add_biotapestry([(6,8,1),(1,7,1)],csv_filename, csv_newfile)
+add_biotapestry([(6,8,1),(3,7,-1)],csv_filename, csv_newfile)
 
 ant_str = convert_biotapestry_to_antimony("../Biotapestry/8gene_test.csv", 8, [1/60, 1, 1/60, 1, 5/60, 5, 1/60])
 r=te.loada(ant_str)
