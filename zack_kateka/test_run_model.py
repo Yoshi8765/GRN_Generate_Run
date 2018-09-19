4c7925d99d26655e95193556b282df7b6b9dbaec
Yoshi8765 <yoshigoto95@gmail.com>
2018-09-19 10:06:59 -0700

    +update data

--- zack_kateka/test_run_model.py
+++ zack_kateka/test_run_model.py
@@ -8,22 +8,22 @@ Created on Fri Sep 14 10:46:56 2018
 from RunModel import run_model
 from GetModel import get_model
 
-model_name='exp2'
+model_name='exp3'
 numGenes = 8
 seednum = 6372 #seed not working
 
 # TODO: fix seed
 # TODO: check to make sure all species in model move concentration
 
-#antStr,biotap_str=get_model(numGenes,model_name=model_name,seed=seednum,export=True)
+antStr,biotap_str=get_model(numGenes,model_name=model_name,seed=seednum,export=False)
 
 
-antStr= open('C:\Users\Yoshi\Documents\GitHub\DREAM-work\zack_kateka\Random_GRNs\exp2\OrigAntimony.txt','r').read()
+#antStr= open('C:\Users\Yoshi\Documents\GitHub\DREAM-work\zack_kateka\Random_GRNs\exp2\OrigAntimony.txt','r').read()
 noiseLevel = 0.05 # put in a percentage. 0.05 = 5%
 tmax=200 # The clean data will have tmax*5 datapoints
 resolution = 10 # How much you want to divide from the clean data (ie 10 => tmax/10 datapoints)
 
-r,res,resN = run_model(antStr,noiseLevel,exportData=[0,'M',True,True,True,True],inputData=[1,tmax,resolution])#,bioTap=biotap_str)
+r,res,resN = run_model(antStr,noiseLevel,exportData=[0,'M',True,True,True,True],inputData=[1,tmax,resolution],bioTap=biotap_str)
 
 print('done!')
 
