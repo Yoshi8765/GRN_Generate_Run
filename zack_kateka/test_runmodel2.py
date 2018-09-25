5c5e58dfd1e5a82ec0cdf814ad3a00b5d3d4c79c
Kateka Seth <katekaseth@gmail.com>
2018-09-25 11:28:44 -0700

    make some testing files to keep

--- zack_kateka/test_runmodel2.py
+++ zack_kateka/test_runmodel2.py
@@ -5,17 +5,17 @@ from RunModel import run_model
 #seed = 19443232
 
 seed = 123468
-ant_str, biotap = get_model(8, seed=seed, model_name="debug_zack")
+ant_str, biotap = get_model(8, seed=seed,export=True)
 
-f = open("debug_biotap.csv", 'w')
-f.write(biotap)
-f.close()
-
-run_model2(ant_str, 0.05,seed=seed, species_type='P', species_nums = [1,2,3,4,5,6,7,8], timepoints = [200,5],
-        showTimePlots=True)
-
-run_model2(ant_str, 0.05,seed=seed, species_type='M', species_nums = [1,2,3,4,5,6,7,8], timepoints = [200,5],
-        showTimePlots=True)
+#f = open("debug_biotap.csv", 'w')
+#f.write(biotap)
+#f.close()
+#
+#run_model2(ant_str, 0.05,seed=seed, species_type='P', species_nums = [1,2,3,4,5,6,7,8], timepoints = [200,5],
+#        showTimePlots=True)
+#
+#run_model2(ant_str, 0.05,seed=seed, species_type='M', species_nums = [1,2,3,4,5,6,7,8], timepoints = [200,5],
+#        showTimePlots=True)
 #run_model2(ant_str, 8, 0.05,seed=seed, species_type='M', species_nums = [1,2,3,4,5,6,7,8], timepoints = [200,5],
 #        showTimePlots=True, perturbs= [("DOWN", [3]), ("KO", [5,6,7])])
 
