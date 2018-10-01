836c0c316b07b14cf88e155e83b35290b5779e9e
Yoshi8765 <yoshigoto95@gmail.com>
2018-10-01 16:54:41 -0700

    =moved everything to GRN_Game, generated the broken network

---
+++ GRN_Game/get_data.py
@@ -0,0 +1,28 @@
+from run_experiments import export_experiments
+import os
+import numpy as np
+
+
+"""
+This section will create a team score file if it doesn't exist.
+***Be sure to change team names in the `team_names` array.***
+***Specify starting amount of credits below.***
+"""
+
+maxcredits = 14000
+
+if os.path.isfile("team_scores.csv") == False:
+    # change team names here. Make sure it matches the google form team names
+    team_names = np.array(["team 1", "team 2", "team 3", "team 4", "team 5", "team 6"])
+    money = np.array([str(maxcredits)]*len(team_names))
+    result = np.vstack((team_names, money))
+    np.savetxt("team_scores.csv", result, fmt="%s",delimiter=",")
+    print("Team score file made!")
+
+"""
+This file will send out the next batch of experimental data.
+Simply ensure that num_genes is the number of genes in the network, and run the script.
+"""
+num_genes = 8
+
+export_experiments(num_genes, sendEmail=True, updateMoney=True)
---
+++ GRN_Game/test_run_model.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 14 10:46:56 2018
+
+@author: Yoshi
+"""
+
+from RunModel import run_model
+from GetModel import get_model
+from GetModel import convert_to_biotapestry
+#from RunModel_2 import run_model2
+
+model_name='exp5'
+numGenes = 8
+seednum = 123468
+
+antStr,biotap_str = get_model(numGenes,model_name=model_name,seed=seednum,export=False)
+
+#antStr= open('C:\Users\Yoshi\Documents\GitHub\DREAM-work\zack_kateka\Random_GRNs\exp3\OrigAntimony_up7.txt','r').read()
+noiseLevel = 0.00 # put in a percentage. 0.05 = 5%
+tmax=200 # minutes. The complete data will have tmax*5 datapoints
+resolution = 1 # The number of minutes between each timepoint you want in your output.
+r,result,resultN = run_model(antStr,noiseLevel,exportData=[0,'P',True,True],inputData=[1,tmax,resolution],showTimePlots=True,bioTap=biotap_str)
+
+#r,res,resN = run_model(antStr,noiseLevel,exportData=[[2,8],'P',True,False,False],inputData=[1,tmax,resolution],showTimePlots=True)
+
+print('done!')
+
+#%% testing biotap removal
+
+from change_biotapestry import remove_biotapestry
+
+#rem = [(8,1),(1,5),(4,7),(5,3),(6,6)]
+rem = [(5,3),(6,6),(8,1),(1,5),(4,7)]
+#rem = [(8,8),(1,5),(5,3)]
+
+#remove_biotapestry(rem,'C:\Users\Yoshi\Documents\GitHub\DREAM-work\zack_kateka\Random_GRNs\exp3\\biotapestry.csv','stestWorking.csv')
+print('done')
